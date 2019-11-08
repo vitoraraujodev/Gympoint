0b35c432da8ca1cd97ed093cc647eb996e64c899
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-11-08 17:19:07 -0300

    Migration e Model de checkin

--- nodejs/src/database/index.js
+++ nodejs/src/database/index.js
@@ -4,10 +4,11 @@ import User from '../app/models/User';
 import Student from '../app/models/Student';
 import Plan from '../app/models/Plan';
 import Registration from '../app/models/Registration';
+import Checkin from '../app/models/Checkin';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Student, Plan, Registration]; // Todos os models a serem carregados
+const models = [User, Student, Plan, Registration, Checkin]; // Todos os models a serem carregados
 
 class Database {
   constructor() {
