f1e93861030bd777b3c4cc75bca2abb85c95956c
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-11-08 12:43:24 -0300

    Model de matrículas

--- nodejs/src/database/index.js
+++ nodejs/src/database/index.js
@@ -3,10 +3,11 @@ import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import Student from '../app/models/Student';
 import Plan from '../app/models/Plan';
+import Registration from '../app/models/Registration';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Student, Plan]; // Todos os models a serem carregados
+const models = [User, Student, Plan, Registration]; // Todos os models a serem carregados
 
 class Database {
   constructor() {
