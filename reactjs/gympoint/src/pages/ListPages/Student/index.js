593f355523ac37f0d6183257fb655c2a90fd6986
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-12-25 05:34:08 -0300

    Requisições autenticadas

--- reactjs/gympoint/src/pages/ListPages/Student/index.js
+++ reactjs/gympoint/src/pages/ListPages/Student/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+import api from '~/services/api';
+
 export default function StudentList() {
+  api.get('students');
   return <h1>StudentList</h1>;
 }
--- reactjs/gympoint/src/store/modules/auth/sagas.js
+++ reactjs/gympoint/src/store/modules/auth/sagas.js
@@ -19,4 +19,17 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
