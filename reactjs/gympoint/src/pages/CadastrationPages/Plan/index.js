7729969a66cb4da0cae9468eca1b60494d7a320d
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-12-30 15:53:43 -0300

    Estilização da página de cadastro de planos

--- reactjs/gympoint/src/pages/CadastrationPages/Plan/index.js
+++ reactjs/gympoint/src/pages/CadastrationPages/Plan/index.js
@@ -1,7 +1,48 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Input } from '@rocketseat/unform';
+
+import { Container, CadastrationForm } from './styles';
+
+import history from '~/services/history';
 
 export default function PlanCadastration() {
-  return <h1>PlanCadastration</h1>;
+  function handleBack(route) {
+    history.push(route);
+  }
+
+  return (
+    <Container>
+      <div>
+        <strong>Cadastro de plano</strong>
+        <aside>
+          <button type="button" onClick={() => handleBack('/plans')}>
+            VOLTAR
+          </button>
+          <button type="button">SALVAR</button>
+        </aside>
+      </div>
+      <CadastrationForm>
+        <strong>TÍTULO DO PLANO</strong>
+        <Input name="title" />
+
+        <section>
+          <article>
+            <strong>DURAÇÃO(meses)</strong>
+            <Input name="duration" />
+          </article>
+          <article>
+            <strong>PREÇO MENSAL</strong>
+            <Input name="price" />
+          </article>
+          <article>
+            <strong>PREÇO TOTAL</strong>
+            <span>
+              <input readOnly />
+            </span>
+          </article>
+        </section>
+      </CadastrationForm>
+    </Container>
+  );
 }
