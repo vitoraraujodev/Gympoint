b16ca5d818c3d1a1cca48da1a7dad7935fc7f5b8
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-01-03 14:22:34 -0300

    Ajuste na API (Já realizado mas não commitado)

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 import routes from './routes';
 
 import './database';
@@ -6,7 +8,8 @@ import './database';
 class App {
   constructor() {
     this.server = express();
-
+    // Aqui coloca o endereço da aplicação React (cors({origin: 'https://...'}))
+    this.server.use(cors());
     this.middlewares();
     this.routes();
   }
